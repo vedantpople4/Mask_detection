d5ed3bf0b0724de983a9023b3c1fd95fc28c9952
Vedant pople <vedantpople4@gmail.com>
2020-12-15 15:19:12 +0530

    create train_mask_detector.py

---
+++ train_mask_detector.py
@@ -0,0 +1,53 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+INIT_LR = 1e-4
+EPOCHS = 20
+BS = 32
+
+DIRECTORY = r"D:\Projects\Mask_detection\dataset"
+CATEGORIES = ["with_mask", "without_mask"]
+
+print("Loading Image from Directory")
+
+data =[]
+labels = []
+
+for category in CATEGORIES:
+    path = os.path.join(DIRECTORY, category)
+    for img in os.listdir(path):
+        img_path = os.path.join(path,img)
+        image = load_img(img_path, target_size=(224,224))
+        image = img_to_array(image)
+        image = preprocess_input(image)
+        
+        data.append(image)
+        labels.append(category)
+        
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+(xtrain, xtest, ytrain, ytest) = train_test_split(data, labels, test_size = 0.20, stratify = labels, random_state = 101)
+
