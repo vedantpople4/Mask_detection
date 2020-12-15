45440f7d906122c2f4712b40608296c35af0575c
Vedant pople <vedantpople4@gmail.com>
2020-12-15 22:35:16 +0530

    complete train_mask_detector.py

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -38,7 +38,6 @@ for category in CATEGORIES:
         image = load_img(img_path, target_size=(224,224))
         image = img_to_array(image)
         image = preprocess_input(image)
-        
         data.append(image)
         labels.append(category)
         
@@ -50,4 +49,72 @@ data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
 (xtrain, xtest, ytrain, ytest) = train_test_split(data, labels, test_size = 0.20, stratify = labels, random_state = 101)
+
+aug = ImageDataGenerator(rotation_range = 20, zoom_range = 0.15, width_shift_range= 0.2, height_shift_range=0.2, shear_range=0.15, horizontal_flip=True, fill_mode="nearest")
+
+baseModel = MobileNetV2(weights="imagenet", include_top=False,
+	input_tensor=Input(shape=(224, 224, 3)))
+
+# construct the head of the model that will be placed on top of the
+# the base model
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(128, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+
+# place the head FC model on top of the base model (this will become
+# the actual model we will train)
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+# loop over all layers in the base model and freeze them so they will
+# *not* be updated during the first training process
+for layer in baseModel.layers:
+	layer.trainable = False
+
+# compile our model
+print("[INFO] compiling model...")
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+# train the head of the network
+print("[INFO] training head...")
+H = model.fit(
+	aug.flow(xtrain, ytrain, batch_size=BS),
+	steps_per_epoch=len(xtrain) // BS,
+	validation_data=(xtest, ytest),
+	validation_steps=len(xtest) // BS,
+	epochs=EPOCHS)
+
+# make predictions on the testing set
+print("[INFO] evaluating network...")
+predIdxs = model.predict(xtest, batch_size=BS)
+
+# for each image in the testing set we need to find the index of the
+# label with corresponding largest predicted probability
+predIdxs = np.argmax(predIdxs, axis=1)
+
+# show a nicely formatted classification report
+print(classification_report(ytest.argmax(axis=1), predIdxs, target_names=lb.classes_))
+
+# serialize the model to disk
+print("[INFO] saving mask detector model...")
+model.save("mask_detector.model", save_format="h5")
+
+# plot the training loss and accuracy
+N = EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig("plot.png")
+
 
