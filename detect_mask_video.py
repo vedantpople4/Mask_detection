09cf52526ff6bb55f6a6fdcbde92de8c746c2819
Vedant pople <vedantpople4@gmail.com>
2021-04-10 17:51:40 +0530

    update detect_face_mask

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -13,60 +13,37 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	(h, w) = frame.shape[:2]
 	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
 		(104.0, 177.0, 123.0))
-
-
 	faceNet.setInput(blob)
 	detections = faceNet.forward()
 	print(detections.shape)
-
-
 	faces = []
 	locs = []
 	preds = []
-
 	for i in range(0, detections.shape[2]):
-
 		confidence = detections[0, 0, i, 2]
-
-
 		if confidence > 0.5:
-		
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 			(startX, startY, endX, endY) = box.astype("int")
-
-	
 			(startX, startY) = (max(0, startX), max(0, startY))
 			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-
-
 			face = frame[startY:endY, startX:endX]
 			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
 			face = cv2.resize(face, (224, 224))
 			face = img_to_array(face)
 			face = preprocess_input(face)
-
-	
 			faces.append(face)
 			locs.append((startX, startY, endX, endY))
-
-	if len(faces) > 0:
-		
+	if len(faces) > 0:		
 		faces = np.array(faces, dtype="float32")
 		preds = maskNet.predict(faces, batch_size=32)
-
 	return (locs, preds)
 
 prototxtPath = r"face_detector\deploy.prototxt"
 weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
-
-
 maskNet = load_model("mask_detector.model")
-
-
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
-
 
 while True:
 	
@@ -94,8 +71,6 @@ while True:
 
 	if key == ord("q"):
 		break
-
-
 cv2.destroyAllWindows()
 vs.stop()
 
